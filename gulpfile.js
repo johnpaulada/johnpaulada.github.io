d36bafd5558283cee19f1096bb7ccb4ac69c3741
John Paul Ada <johnpaulada@Ferdinand-Mini.local.gap>
2016-09-08 18:31:36 +0800

    Added JS minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@ var gulp      = require('gulp');
 var concat    = require('gulp-concat');
 var cleanCSS  = require('gulp-clean-css');
 var concatCSS = require('gulp-concat-css');
+var uglify    = require('gulp-uglify');
 
 gulp.task('minify-css', function() {
     return gulp.src('src/css/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-js', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('bundle.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
